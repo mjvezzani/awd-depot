ef0b4a4de09c660737edfa2e040f39fd99fbae0f
Michael Vezzani <mjvezzani@gmail.com>
2014-04-21 13:38:53 -0400

    Handle Cart Errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421165839) do
+ActiveRecord::Schema.define(version: 20140421170750) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
